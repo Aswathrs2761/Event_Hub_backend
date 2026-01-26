efccc21e142b297ab3af1ce28293cca7c95f129f
Aswathrs2761 <aswathrs2001@gmail.com>
2026-01-26 21:28:18 +0530

    changes

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use("/api/admin",adminRoute)
 
 const port = process.env.port
 
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
